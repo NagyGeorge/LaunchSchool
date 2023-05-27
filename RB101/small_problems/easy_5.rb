d5ce813be7b81b31806976bd24d058ce1f4b37c4
NagyGeorge <11335538+NagyGeorge@users.noreply.github.com>
2023-05-27 00:55:08 -0500

    finished a problem in easy 5

---
+++ RB101/small_problems/easy_5.rb
@@ -0,0 +1,8 @@
+# Write a method that determines and returns the ASCII string value of a string
+# that is passed in as an argument. The ASCII string value is the sum of the
+# ASCII values of every character in the string. (You may use String#ord to
+# determine the ASCII value of a character.)
+
+def ascii_value(text)
+  text.sum
+end
